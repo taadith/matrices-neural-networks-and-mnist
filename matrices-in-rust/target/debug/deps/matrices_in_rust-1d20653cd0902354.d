07447002c420766bdcba458e8d6685edbd7fa4e3
Aadith_Th <taadith@gmail.com>
2022-06-11 19:56:02 -0500

    Delete matrices-in-rust directory
    Deleted Rust implementation of the matrices project as I lack the experience to work with pointers in Rust at this point in time

--- matrices-in-rust/target/debug/deps/matrices_in_rust-1d20653cd0902354.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/rust-code/matrices-in-rust/target/debug/deps/matrices_in_rust-1d20653cd0902354.rmeta: src/main.rs
-
-/mnt/c/rust-code/matrices-in-rust/target/debug/deps/matrices_in_rust-1d20653cd0902354.d: src/main.rs
-
-src/main.rs:
--- matrices-in-rust/target/debug/matrices-in-rust.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/rust-code/matrices-in-rust/target/debug/matrices-in-rust: /mnt/c/rust-code/matrices-in-rust/src/main.rs
